241bdf3ad2e5930ab39cc615e84531f24a84876a
India Byrne <byrneindia@gmail.com>
2020-02-27 12:26:27 -0700

    Test

---
+++ conditionsandfunctions.js
@@ -0,0 +1,17 @@
+/*
+* Conditionals, Functions, Scope, and Loops.
+ */
+
+//Equals
+let equals = 1 === 1;
+
+//Greater than
+let greaterThan =5 > 1;
+
+//Less than
+let lessThan = 2 <10;
+
+//Greater than
+let greaterThanEq = 5 >= 5;
+
+
